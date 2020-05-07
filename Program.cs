f669e8300822c6c29e9a55fec504e423219ffc92
Yoav Dobrin <37622785+yodobrin@users.noreply.github.com>
2020-05-07 13:27:05 +0300

    initial

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace sqltest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "ydsqlserver.database.windows.net";
+                builder.UserID = "yoav";
+                builder.Password = "4d*JH22#";
+                builder.InitialCatalog = "elta";
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    Console.WriteLine("\nQuery data example:");
+                    Console.WriteLine("=========================================\n");
+
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("SELECT '1' as shuki ");
+                    //sb.Append("FROM [SalesLT].[ProductCategory] pc ");
+                    //sb.Append("JOIN [SalesLT].[Product] p ");
+                    //sb.Append("ON pc.productcategoryid = p.productcategoryid;");
+                    String sql = sb.ToString();
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0}", reader.GetString(0));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
